8e5c7ac1fba850586f56db4660a1ef1b2fe6f11c
Angela Wang <angeladanwang@gmail.com>
2015-04-18 17:58:05 -0700

    Refactor with Sequelize

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,7 @@
 var models = require('../models');
 var bluebird = require('bluebird');
 var _ = require('underscore');
+var db = require('../db');
 
 var headers = {
   'Content-Type': 'application/json',
@@ -15,26 +16,53 @@ module.exports = {
 
   messages: {
     get: function (req, res) {
-     
-      // ask models for messages from database
-      models.messages.get(function(rows){
-        var status = status || 200;
-        res.writeHead(status,headers);
-        if(req.query.where !== undefined) {
-          rows = _.filter(rows, function(message) {
-            return message.roomname === req.query.where.roomname;
-          });
+      var params = req.query || {};
+      params.include = [db.User];
+      db.Message.findAll(params).complete(function (err, result) {
+        console.log(result);
+        if (err) throw err;
+        res.set(headers);
+        res.status(200);
+        if (result[0].dataValues.User) {
+          console.log(result[0].dataValues.User.dataValues);
+          result[0].dataValues.username = result[0].dataValues.User.dataValues.username;
         }
-        var data = {results: rows};
-        res.end(JSON.stringify(data));
-      });    
+        res.json({results: result});
+      })
+
+
+      // models.messages.get(function(rows){
+      //   var status = status || 200;
+      //   res.writeHead(status,headers);
+      //   if(req.query.where !== undefined) {
+      //     rows = _.filter(rows, function(message) {
+      //       return message.roomname === req.query.where.roomname;
+      //     });
+      //   }
+      //   var data = {results: rows};
+      //   res.end(JSON.stringify(data));
+      // });    
     }, // a function which handles a get request for all messages
     
     post: function (req, res) {
-      models.messages.post(req.body, function () {
-        res.writeHead(201,headers);
-        res.end(JSON.stringify({posted: true}));
+      console.log(req.body);
+      db.User.findOrCreate({where: {username: req.body.username}})
+      .done(function (err, result) {
+        if (err) throw err;
+        db.Message.create({
+          text: req.body.text,
+          roomname: req.body.roomname,
+          userid: result[0].dataValues.id
+        });
+        res.set(headers);
+        res.status(201);
+        res.json(result);
       });
+
+      // models.messages.post(req.body, function () {
+      //   res.writeHead(201,headers);
+      //   res.end(JSON.stringify({posted: true}));
+      // });
     },
 
     options: function(req, res) {
--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,35 @@
 var mysql = require('mysql');
-
+var Sequelize = require('sequelize');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
-var dbConnection = mysql.createConnection({
-    user: "root",
-    password: "",
-    database: "chat"
+// var dbConnection = mysql.createConnection({
+//     user: "root",
+//     password: "",
+//     database: "chat"
+// });
+
+// dbConnection.connect();
+
+// module.exports = dbConnection;
+
+var sequelize = new Sequelize("chat", "root", "");
+
+var User = sequelize.define('User', {
+  username: Sequelize.STRING 
 });
 
-dbConnection.connect();
+var Message = sequelize.define('Message', {
+  userid: Sequelize.INTEGER,
+  text: Sequelize.STRING,
+  roomname: Sequelize.STRING
+});
 
-module.exports = dbConnection;
+User.sync();
+Message.sync();
 
+User.hasMany(Message);
+Message.belongsTo(User);
+
+exports.User = User;
+exports.Message = Message;
